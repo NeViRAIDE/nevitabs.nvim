9c2493ca73d18f520541e7d24c048fe0cd49e153
RAprogramm <andrey.rozanov.vl@gmail.com>
2024-07-13 16:18:51 +0900

    which-key v3

--- lua/nevitabs/whichkey.lua
+++ lua/nevitabs/whichkey.lua
@@ -1,112 +1,124 @@
 local wk = require('which-key')
 local i = require('stigmata.utils').icon
 
-wk.register({
-  ['<tab>'] = {
-    function() require('nevitabs').tabuflineNext() end,
-    'Goto to the next buffer',
+wk.add({
+  {
+    '<tab>',
+    rhs = function() require('nevitabs').tabuflineNext() end,
+    desc = 'Goto to the next buffer',
   },
-  ['<S-tab>'] = {
-    function() require('nevitabs').tabuflinePrev() end,
-    'Goto to the previous buffer',
+  {
+    '<S-tab>',
+    rhs = function() require('nevitabs').tabuflinePrev() end,
+    desc = 'Goto to the previous buffer',
   },
-  ['<leader>t'] = { name = 'Tabs' .. i('t', 'tab-external', 1) },
-  ['<leader>tc'] = {
-    name = 'Close' .. i('', 'trash', 1),
-    c = {
-      '<cmd>tabclose<cr>',
-      'Close current',
-    },
-    a = {
-      '<cmd>tabonly<cr>',
-      'Close all, except the current one',
-    },
+  { '<leader>t', group = 'Tabs', icon = i('t', 'tab-external') },
+  { '<leader>tc', group = 'Close', icon = i('', 'trash') },
+  {
+    '<leader>tcc',
+    '<cmd>tabclose<cr>',
+    desc = 'Close current',
   },
-  ['<leader>tp'] = {
+  {
+    '<leader>tca',
+    '<cmd>tabonly<cr>',
+    desc = 'Close all, except the current one',
+  },
+  {
+    '<leader>tp',
     '<cmd>tabprevious<cr>',
-    'Go to previous',
+    desc = 'Go to previous',
   },
-  ['<leader>tn'] = {
+  {
+    '<leader>tn',
     '<cmd>tabnext<cr>',
-    'Go to next',
+    desc = 'Go to next',
   },
-  ['<leader>tN'] = {
+  {
+    '<leader>tN',
     '<cmd>tabnew<cr>',
-    'New',
+    desc = 'New',
   },
-  ['<leader>tm'] = {
-    name = 'Move',
-    r = {
-      '<cmd>+tabmove<cr>',
-      'Move to the right',
-    },
-    l = {
-      '<cmd>-tabmove<cr>',
-      'Move to the left',
-    },
+  { '<leader>tm', group = 'Move' },
+  {
+    '<leader>tmr',
+    '<cmd>+tabmove<cr>',
+    desc = 'Move to the right',
   },
-  ['<leader>tr'] = {
-    function()
+  {
+    '<leader>tml',
+    '<cmd>-tabmove<cr>',
+    desc = 'Move to the left',
+  },
+  {
+    '<leader>tr',
+    rhs = function()
       local number_of_tabs = vim.fn.tabpagenr('$')
-      if number_of_tabs > 1 then
-        require('nevitabs.set_tab_name')()
-      end
+      if number_of_tabs > 1 then require('nevitabs.set_tab_name')() end
     end,
-    'Rename',
+    desc = 'Rename',
   },
-  ['<leader>tt'] = {
-    function() require('nevitabs.modules.components').toggle_tabs() end,
-    'Toggle tabs panel',
+  {
+    '<leader>tt',
+    rhs = function() require('nevitabs.modules.components').toggle_tabs() end,
+    desc = 'Toggle tabs panel',
   },
 
-  ['<leader>b'] = { name = 'Buffers' .. i('b', 'tmux', 1) },
-  ['<leader>bd'] = {
-    name = 'Delete' .. i('', 'trash', 1),
-    c = {
-      function() require('nevitabs').close_buffer() end,
-      'Current',
-    },
-    a = {
-      function() require('nevitabs').closeOtherBufs() end,
-      'All, except current',
-    },
-    A = {
-      function()
-        require('nevitabs').closeAllBufs()
-        require('nevitabs').close_buffer()
-      end,
-      'All',
-    },
-    l = {
-      function() require('nevitabs').closeBufs_at_direction('left') end,
-      'All to the left of the current one',
-    },
-    r = {
-      function() require('nevitabs').closeBufs_at_direction('right') end,
-      'All to the right of the current one',
-    },
+  { '<leader>b', group = 'Buffers', icon = i('b', 'tmux') },
+  { '<leader>bd', group = 'Delete', icon = i('', 'trash') },
+  {
+    '<leader>bdc',
+    rhs = function() require('nevitabs').close_buffer() end,
+    desc = 'Current',
   },
-  ['<leader>bm'] = {
-    name = 'Move',
-    l = {
-      function() require('nevitabs').move_buf(-1) end,
-      'Move to the left',
-    },
-    r = {
-      function() require('nevitabs').move_buf(1) end,
-      'Move to the right',
-    },
+  {
+    '<leader>bda',
+    rhs = function() require('nevitabs').closeOtherBufs() end,
+    desc = 'All, except current',
   },
-  ['<leader>bl'] = {
+  {
+    '<leader>bdA',
+    rhs = function()
+      require('nevitabs').closeAllBufs()
+      require('nevitabs').close_buffer()
+    end,
+    desc = 'All',
+  },
+  {
+    '<leader>bdl',
+    rhs = function() require('nevitabs').closeBufs_at_direction('left') end,
+    desc = 'All to the left of the current one',
+  },
+  {
+    '<leader>bdr',
+    rhs = function() require('nevitabs').closeBufs_at_direction('right') end,
+    desc = 'All to the right of the current one',
+  },
+  { '<leader>bm', group = 'Move' },
+  {
+    '<leader>bml',
+    rhs = function() require('nevitabs').move_buf(-1) end,
+    desc = 'Move to the left',
+  },
+  {
+    '<leader>bmr',
+    rhs = function() require('nevitabs').move_buf(1) end,
+    desc = 'Move to the right',
+  },
+  {
+    '<leader>bl',
     '<cmd>Neotree buffers focus float toggle<cr>',
-    'Toggle buffers list (Neotree)' .. i('', 'list-unordered', 1),
+    desc = 'Toggle buffers list (Neotree)',
+    icon = i('', 'list-unordered'),
   },
-  ['<leader>bn'] = {
-    function() require('nevitabs').tabuflineNext() end,
-    'Go to the next',
+  {
+    '<leader>bn',
+    rhs = function() require('nevitabs').tabuflineNext() end,
+    desc = 'Go to the next',
   },
-  ['<leader>bp'] = {
-    function() require('nevitabs').tabuflinePrev() end,
-    'Go to the previous',
+  {
+    '<leader>bp',
+    rhs = function() require('nevitabs').tabuflinePrev() end,
+    desc = 'Go to the previous',
   },
 })
